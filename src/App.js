c2f9bbe104e0c916246f13ae7b50e62dedff2910
kevinsims1 <kevinsims1@outlook.com>
2019-12-06 09:26:17 -0600

    tweet form made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+
+//Components
 import TweetForm from './components/tweetform.js'
 import AllTweets from './components/alltweets.js'
 
 class App extends React.Component{
+  // state ={
+  //   name: '',
+  //   tweet: ''
+  // }
+
+  // onSubmit(e){
+  //   e.preventDefault()
+  //   url = fetch()
+  // }
   render(){
     return (
       <div className="App">
--- src/components/tweetform.js
+++ src/components/tweetform.js
@@ -1,9 +1,21 @@
 import React from 'react'
+//Bootstrap
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap'
 
 export default class TweetForm extends React.Component {
     render(){
         return(
-          <h1>Hello</h1>
+          <Form>
+            <FormGroup onSubmit={this.onSubmit}>
+                <Label>Username</Label>
+                <Input type="text" name="username" placeholder="Enter your username"/>
+
+                <Label>Tweet</Label>
+                <Input type="textarea" name="tweet" placeholder="Enter your tweet here"/>
+
+                <Button type="submit">Submit</Button>
+            </FormGroup>
+          </Form>
         )
     }
 }
