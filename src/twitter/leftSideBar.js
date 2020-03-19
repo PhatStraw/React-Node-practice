a16dfb32b5359ee79085b70b12497e66e0f3c8f4
kevinsims1 <kevinsims1@outlook.com>
2020-03-19 11:47:39 -0700

    styles left nav

--- src/twitter/leftSideBar.js
+++ src/twitter/leftSideBar.js
@@ -1,22 +1,36 @@
 import React from 'react'
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink, Button } from 'reactstrap';
 import '../styles/left.css'
-
+import { FaTwitter, FaHome, FaHashtag, FaRegBell, FaWhatsapp, FaBookmark } from "react-icons/fa";
+import { IconContext } from "react-icons";
 export default function Left() {
     return (
-        <Nav vertical>
-            <NavItem>
-                <NavLink color="primary" href="#">Link</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#">Link</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#">Another Link</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#">Link</NavLink>
-            </NavItem>
-        </Nav>
+        <div className="left-container">
+            
+            <IconContext.Provider value={{ style: {fontSize: '30px', color: "rgb(0, 123, 255)"}}}>
+                <div>
+                    <FaTwitter />
+                </div>
+            </IconContext.Provider>
+            <Nav vertical>
+                <NavItem style={{ margin: '7px' }}>
+                    <NavLink color="primary" href="#" style={{ fontSize: '22px' }}><FaHome /> Home</NavLink>
+                </NavItem>
+                <NavItem style={{ margin: '7px' }}>
+                    <NavLink href="#" style={{ fontSize: '22px' }}><FaHashtag /> Explore</NavLink>
+                </NavItem>
+                <NavItem style={{ margin: '7px' }}>
+                    <NavLink href="#" style={{ fontSize: '22px' }}><FaRegBell /> Notifications</NavLink>
+                </NavItem>
+                <NavItem style={{ margin: '7px' }}>
+                    <NavLink href="#" style={{ fontSize: '22px' }}><FaWhatsapp /> Messages</NavLink>
+                </NavItem>
+                <NavItem style={{ margin: '7px' }}>
+                    <NavLink href="#" style={{ fontSize: '22px' }}><FaBookmark /> Bookmarks</NavLink>
+                </NavItem>
+                <Button color="primary">Tweet</Button>
+            </Nav>
+        </div>
+
     )
 }
