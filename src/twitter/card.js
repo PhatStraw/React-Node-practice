33eb2c48d887ef2c9caa759e3cc1adfb61c13d9d
kevinsims1 <kevinsims1@outlook.com>
2020-03-20 16:22:54 -0700

    started right side navigation

--- src/twitter/card.js
+++ src/twitter/card.js
@@ -5,21 +5,25 @@ import '../styles/card.css'
 export default function CardComp(props) {
     return (
         <div>
-            {
-                props.data.tweets.map(tweet => (
-                    <Card body outline color="secondary">
-                        <CardTitle>{tweet.user_id}</CardTitle>
-                        <CardText>{tweet.tweet}</CardText>
-                        <div style={{display: 'flex', flexDirection: 'row'}}>
-                            <Button style={{margin: '10px', width: '20%', fontSize: '8px'}} color="primary">
-                                 Likes {tweet.likes}
-                            </Button>
-                            <Button style={{margin: '10px', width: '20%', fontSize: '8px'}} color="primary">Retweets   {tweet.retweets}</Button>
-                            <Button style={{margin: '10px', width: '20%', fontSize: '8px'}} color="primary">Comments   {tweet.comments}</Button>
-                        </div>
-                    </Card>
-                ))
-            }
+            <h1>hi</h1>
+            <div>
+                {
+                    props.data.tweets.map(tweet => (
+                        <Card body outline color="secondary">
+                            <CardTitle>{tweet.user_id}</CardTitle>
+                            <CardText>{tweet.tweet}</CardText>
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <Button style={{ margin: '10px', width: '20%', fontSize: '8px' }} color="primary">
+                                    Likes {tweet.likes}
+                                </Button>
+                                <Button style={{ margin: '10px', width: '20%', fontSize: '8px' }} color="primary">Retweets   {tweet.retweets}</Button>
+                                <Button style={{ margin: '10px', width: '20%', fontSize: '8px' }} color="primary">Comments   {tweet.comments}</Button>
+                            </div>
+                        </Card>
+                    ))
+                }
+            </div>
         </div>
+
     )
 }
--- src/twitter/rightSideBar.js
+++ src/twitter/rightSideBar.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Card, ListGroup, ListGroupItem } from 'reactstrap'
+
 import '../styles/right.css'
 
 export default function Right() {
     return (
-        <ul className="list-group">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Morbi leo risus</li>
-            <li className="list-group-item">Porta ac consectetur ac</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-        </ul>
+        <div className="right-wrapper">
+                <ListGroup flush>
+                <h3>Trends For You</h3>
+                    <ListGroupItem disabled tag="a" href="#">Cras justo odio</ListGroupItem>
+                    <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
+                    <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
+                    <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
+                    <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+        </div>
+
     )
 }
